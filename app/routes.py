830fe65a1d495876271b292a615845126000a429
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-11 11:50:25 -0600

    installed sql

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
 
 @app.route('/')
@@ -11,7 +11,19 @@ def index():
 def posts():
     return 'These are the posts!'
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET','POST'])
 def signup():
     form = SignUpForm()
+    print(form.data)
+    if form.validate_on_submit():
+        print('From Submitter and Validated')
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        if username == 'bs':
+            flash('The user already exists', 'danger')
+            return render_template('signup.html', form=form)
+        flash('Thank you for signing up!', 'success')
+        return redirect(url_for('index'))
     return render_template('signup.html', form=form)
