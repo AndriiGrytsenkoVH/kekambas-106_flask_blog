c6417dd456347382b8cd590e05b24136bee49027
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-11 12:40:33 -0600

    about to merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-duper-secret(not)'
+app.config.from_object(Config)
+
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # import all of the routes from the routes file into the current folder
 from . import routes
