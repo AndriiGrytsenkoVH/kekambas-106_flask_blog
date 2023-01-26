1f98f91324b016db078322f482a0b52f6b0fb14a
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2023-01-26 08:33:57 -0600

    added user api

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,6 +1,6 @@
 from flask import request
 from . import api
-from app.models import Post
+from app.models import Post, User
 
 @api.route('/')
 def index():
@@ -44,3 +44,42 @@ def create_post():
     new_post = Post(title=title, body=body, user_id=user_id)
     # Return the new post as a JSON response
     return new_post.to_dict(), 201
+
+#####################################################################################################################################################
+
+# Endpoint to create a new user
+@api.route('/users', methods=['POST'])
+def create_user():
+    # Check to see that the request sent a request body that is JSON
+    if not request.is_json:
+        return {'error': 'Your request content-type must be application/json'}, 400
+    # Get the data from the request body
+    data = request.json
+    # Validate the incoming data
+    for field in ['email', 'username', 'password']:
+        if field not in data:
+            # If the field is not in the request body, throw an error saying they are missing that field
+            return {'error': f"{field} must be in request body"}, 400
+    
+    # pull the fields from the request data
+    email = data.get('email')
+    username = data.get('username')
+    password = data.get('password')
+
+    # Query our user table to see if there are any users with either username or email from form
+    check_user = User.query.filter( (User.username == username) | (User.email == email) ).first()
+    # If the query comes back with any results
+    if check_user:
+        return {'error': 'A user with that email and/or username already exists.'}, 400
+
+    # Create a new User instance with data from request
+    new_user = User(email=email, username=username, password=password)
+    # Return the new post as a JSON response
+    return new_user.to_dict(), 201
+
+
+# Endpoint to get a single user
+@api.route('/users/<int:user_id>')
+def get_user(post_id):
+    user = User.query.get_or_404(post_id)
+    return user.to_dict()
